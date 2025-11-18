88aa67a13d193fa220edd3f107063b6334daf0e0
sahiba <25sahiba03@gmail.com>
2025-11-18 20:20:12 +0530

    add js style for all icon

--- second.js
+++ second.js
@@ -4,7 +4,6 @@ const firstlist = document.getElementById("firstlist");
 const secondlist = document.getElementById("secondlist");
 const firstarr = [];
 const secondarr = [];
-
 button.addEventListener("click",clickfuntion);
 function clickfuntion(){
     const text = input.value.trim();
@@ -14,11 +13,11 @@ function clickfuntion(){
         input.value = "";
         firstListfunction(firstarr);
     }
-}; //click function is ended
+};
 function firstListfunction(firstarr){
     firstlist.innerHTML = "";
        for( let i=0; i<firstarr.length; i++){ 
-       firstlist.insertAdjacentHTML(
+         firstlist.insertAdjacentHTML(
             "beforeend",          
               `
               <li class=liItems> 
@@ -29,67 +28,61 @@ function firstListfunction(firstarr){
               <span class="remove"> x </span>
               </li>
                `              
-          );  
-                        
+          ); 
     const rem = document.querySelectorAll(".remove");
     const edi = document.querySelectorAll(".addicon");
 
-           rem[i].addEventListener("click",deletefunction);
+     rem[i].addEventListener("click",deletefunction);
              function deletefunction(){
             firstarr.splice(i,1);
              firstListfunction(firstarr);
          };
 
-          edi[i].addEventListener("click",editfunction);
+         edi[i].addEventListener("click",editfunction);
           function editfunction(){
-            const secondListItem = firstarr[i];
-            secondarr.push(secondListItem);
+            const secondlistitem = firstarr[i];
+            secondarr.push(secondlistitem);
              firstarr.splice(i,1);
-            secondlist.insertAdjacentHTML(
+             firstListfunction(firstarr);
+             secondlistfunction(secondarr);
+          }          
+}
+
+};
+function secondlistfunction(secondarr){
+    secondlist.innerHTML = "";
+   for(let i=0; i<secondarr.length; i++){
+       secondlist.insertAdjacentHTML(
             "beforeend",          
               `
               <li class=liItems2> 
               <div class="input-logo2">
               <span class="addicon2"><i class="fa-sharp fa-regular fa-circle-check"></i></span>
-              <span class="text2">${secondListItem}</span>
+              <span class="text2">${secondarr[i]}</span>
               </div>             
               <span class="remove2"> x </span>
               </li>
                `              
           );
-           firstListfunction(firstarr);
-         const rem2 = document.querySelectorAll(".remove2");
-        const add2 = document.querySelectorAll(".addicon2");
-        
-          rem2[i].addEventListener("click", rem2fun);
-          function rem2fun(){
-            rem2[i].parentElement.remove();
+              const rem2 = document.querySelectorAll(".remove2");
+              const edit2 = document.querySelectorAll(".addicon2");
+
+             rem2[i].addEventListener("click",delete2function);
+             function delete2function(){
             secondarr.splice(i,1);
+             secondlistfunction(secondarr);
              firstListfunction(firstarr);
-          
-          };
-          add2[i].addEventListener("click",add2fun);
-           function add2fun(){
-            const listcopy = secondarr[i];
-             firstarr.push(listcopy);
-             add2[i].parentElement.parentElement.remove();
-             secondarr.splice(i,1);
+         };
+         edit2[i].addEventListener("click",edit2function);
+        function edit2function(){
+             const firstlistitem = secondarr[i];  
+              firstarr.push(firstlistitem);
+              secondarr.splice(i,1);
+              secondlistfunction(secondarr);
              firstListfunction(firstarr);
-           };
-          
-        }         
-       }
-              
-       };
+
+        };
         
-
-        
-            
-    
-     
-       
-          
-
- 
-
-
+   }
+   
+}
