0b065694eb4eaed055a30a4204b7d82370b31b3c
sahiba <25sahiba03@gmail.com>
2025-12-18 20:13:27 +0530

    add event with task icons

--- TodoTask.js
+++ TodoTask.js
@@ -2,8 +2,7 @@
 var allTaskListContainer = 20;
 var slids = document.querySelector("#taskContainer");
 document.querySelector("#sliderRightBtn").addEventListener("click",()=>{
-    alert("yes clicking right btn");
-  index++;
+    index++;
    if(index > 1){
     index = 0;
   }
@@ -11,8 +10,7 @@ document.querySelector("#sliderRightBtn").addEventListener("click",()=>{
 
 });
 document.querySelector("#sliderLeftBtn").addEventListener("click",()=>{
-    alert("yes clicking left btn");
-  index--;
+    index--;
   if(index < 0){
    index = 1;
   }
@@ -25,10 +23,6 @@ document.querySelector("#sliderLeftBtn").addEventListener("click",()=>{
 const taskInput = document.querySelector("#taskInput");
 const addBtn = document.querySelector("#addBtn");
 const dueDate = document.querySelector("#dueDate");
-
-const searchInput = document.querySelector("#searchInput");
-const searchBtn = document.querySelector("#searchBtn");
-const clearBtn = document.querySelector("#clearBtn");
 
 const todoList = document.querySelector("#todoList");
 const progressList = document.querySelector("#progressList");
@@ -49,9 +43,8 @@ let updateTodoTaskIndex = -1;
 
 addBtn.addEventListener("click", handleAddEditTask);
 function handleAddEditTask() {
-  alert("btn is clicking");
   var inputboxText = taskInput.value.trim();
-  var createDate = new Date();
+  var createDatevalue = new Date().toISOString().split("T");
   var dueDatevalue = dueDate.value;
   var updatedate = "not updated";
   if (inputboxText == "")
@@ -59,12 +52,10 @@ function handleAddEditTask() {
 
   if (updateTodoTaskIndex == -1)
      {
-    todoListData.push({title:inputboxText,createDate:createDate,update:updatedate,dueDate:dueDatevalue});
+    todoListData.push({title:inputboxText,createDate:createDatevalue,update:updatedate,due:dueDatevalue});
     taskInput.value = " ";
     dueDate.value = " ";
-      createDate = " ";    
-    // updatedate = " ";
-     searchInput.value = "";
+    searchInput.value = "";
     createTodoList();
     handleSearchListItem();
       
@@ -72,8 +63,7 @@ function handleAddEditTask() {
        else {
     todoList.children[updateTodoTaskIndex].querySelector(".todoTask" ).textContent = taskInput.value;
     todoListData[updateTodoTaskIndex].title = taskInput.value;
-    //  todoListData[updateTodoTaskIndex].update = "updated";
-    taskInput.value = "";
+     taskInput.value = "";
      handleSearchListItem();
     updateTodoTaskIndex = -1;      
   }
@@ -89,7 +79,7 @@ function createTodoList() {
         <div class="todoTask">${todoListData[i].title}</div>
         <div class="todoCreateDate">Created: ${todoListData[i].createDate}</div>
          <div class="todoCreateDate"> ${todoListData[i].update}</div>
-        <div class="todoDueDate">Due: ${todoListData[i].dueDate}</div>
+        <div class="todoDueDate">Due: ${todoListData[i].due}</div>
         </div>
         <div class="todoiconContainer">
         <div class="todoEditButton"><i class="fa-solid fa-pencil"></i></div>
@@ -115,10 +105,10 @@ function createTodoList() {
     function todoMoveList()
      {
       var progressListTitle = todoListData[i].title;
-      var progressListCreate = todoListData[i].CreateDate;
-      var progressListDue = todoListData[i].dueDate;
+      var progressListCreate = todoListData[i].createDate;
+      var progressListDue = todoListData[i].due;
 
-       progressListData.push({title:progressListTitle,createDate:progressListCreate,dueDate:progressListDue});
+       progressListData.push({title:progressListTitle,createDate:progressListCreate,due:progressListDue});
         todoListData.splice(i, 1);
         createTodoList();
        createProgressList();
@@ -144,7 +134,7 @@ function createProgressList() {
         <div class="progressListDetail">
         <div class="progressTask">${progressListData[i].title}</div>
         <div class="progressCreateDate">Created: ${progressListData[i].createDate}</div>
-        <div class="progressDueDate">Due: ${progressListData[i].dueDate}</div>
+        <div class="progressDueDate">Due: ${progressListData[i].due}</div>
         </div>
         <div class="progressIconContainer">
         <div class="progressMoveButton"><i class="fa-solid fa-arrow-right-long"></i></div>
@@ -167,12 +157,11 @@ function createProgressList() {
  
      progressListItem[i].querySelector(".progressMoveButton").addEventListener("click", progressMoveLists);
      function progressMoveLists(){
-      alert("yes clicking");
       var completedListTitle = progressListData[i].title;
       var completedListCreate = progressListData[i].createDate;
-      // var completedListDate = new Date();
+      var completedListCompleted = "completed";
       
-      completedListData.push({title:completedListTitle,createDate:completedListCreate});
+      completedListData.push({title:completedListTitle,createDate:completedListCreate,compledDate:completedListCompleted});
        progressListData.splice(i, 1);     
       createProgressList();
       createCompletedList();
@@ -192,33 +181,33 @@ function createCompletedList() {
         <div class="completedListDetail">
         <div class="completedTask">${completedListData[i].title}</div>
         <div class="completedCreateDate">Created: ${completedListData[i].createDate}</div>
-        <div class="completedDueDate">Due: ${completedListData[i].comletedDate}</div>
+        <div class="completedDueDate">Completed: ${completedListData[i].comletedDate}</div>
         </div>
-        <div class="progressIconContainer">
-        <div class="completedMoveButton"><i class="fa-solid fa-arrow-right-long"></i></div>
-        <div class=completedDeleteButton"><i class="fa-regular fa-trash-can"></i></div>
+        <div class="completedIconContainer">
+        <div class="completedMoveButton"><i class="fa-solid fa-arrow-right"></i></div>
+        <div class="completedDeleteButton"><i class="fa-regular fa-trash-can"></i></div>
         </div>
         </li>
         `
     );
   }
-  const completedlistItem = document.querySelectorAll(".completedlistItem");
+  const completedListItem = document.querySelectorAll(".completedListItem");
   for (let i = 0; i < completedListData.length; i++)
      {
-    completedlistItem[i].querySelector(".completedDeleteButton").addEventListener("click", completedDeleteList);
+    completedListItem[i].querySelector(".completedDeleteButton").addEventListener("click", completedDeleteList);
       function completedDeleteList() 
       {
-      completedListData.splice(i, 1);
+        completedListData.splice(i, 1);
         createCompletedList();
     }
-    completedlistItem[i].querySelector(".completedMoveButton").addEventListener("click", completedAddList);
+    completedListItem[i].querySelector(".completedMoveButton").addEventListener("click", completedAddList);
     function completedAddList(){ 
 
      var signListTitle = completedListData[i].title;
-      var signListExpiry = "expiry";
-      var signCompleted = "completed";
+      var signListExpiry = "expiry date";
+      var signCompleted = "completed date";
 
-       progressListData.push({title:signListTitle,expiry:signListExpiry,completed:signCompleted});
+       signListData.push({title:signListTitle,expiry:signListExpiry,completed:signCompleted});
         completedListData.splice(i, 1);
         createCompletedList();
        createSignList();
@@ -232,23 +221,23 @@ function createCompletedList() {
   for (let i = 0; i < signListData.length; i++) {
         signList.insertAdjacentHTML(
       "beforeend",
-      `<li class="completedListItem">
-        <div class="completedListDetail">
-        <div class="completedTask">${completedListData[i].title}</div>
-        <div class="completedCreateDate">Created: ${completedListData[i].expiry}</div>
-        <div class="completedDueDate">Due: ${completedListData[i].completed}</div>
+      `<li class="signListItem">
+        <div class="signListDetail">
+        <div class="signTask">${signListData[i].title}</div>
+        <div class="signExpiryDate">Expiry: ${signListData[i].expiry}</div>
+        <div class="signCompletedDate">Completed: ${signListData[i].completed}</div>
         </div>
-        <div class="progressIconContainer">
-         <div class=completedDeleteButton"><i class="fa-regular fa-trash-can"></i></div>
+        <div class="signIconContainer">
+         <div class="signDeleteButton"><i class="fa-regular fa-trash-can"></i></div>
         </div>
         </li>
         `
     );
   }
-   const totalSignlist = document.querySelectorAll(".signListItem");
+   const signListItem = document.querySelectorAll(".signListItem");
   for (let i = 0; i < signListData.length; i++)    
      {
- totalSignlist[i].querySelector(".signDeleteButton").addEventListener("click",signDeletList);
+ signListItem[i].querySelector(".signDeleteButton").addEventListener("click",signDeletList);
  function signDeletList(){
 signListData.splice(i,1);
 createSignList();
@@ -258,88 +247,98 @@ createSignList();
 };
 
 
-  function createDelayList(){
+//   function createDelayList(){
     
-delayList.innerHTML = "";
-  for (let i = 0; i < delayListData.length; i++) {
-    delayList.insertAdjacentHTML(
-      "beforeend",
-      `<li class="delayListItem">
-        <div class="iconTextOfDelayList">
-        <span class="delayAddButton"><i class="fa-solid fa-right-from-bracket"></i></span>
-        <span class="listText">${delayListData[i]}</span>
-        </div>        
-        <div class="delayDeleteButton"> <i class="fa-solid fa-xmark"></i></div>        
-        </li>
-        `
-    );
-  }
+// delayList.innerHTML = "";
+//   for (let i = 0; i < delayListData.length; i++) {
+//     delayList.insertAdjacentHTML(
+//       "beforeend",
+//       `<li class="delayListItem">
+//         <div class="iconTextOfDelayList">
+//         <span class="delayAddButton"><i class="fa-solid fa-right-from-bracket"></i></span>
+//         <span class="listText">${delayListData[i]}</span>
+//         </div>        
+//         <div class="delayDeleteButton"> <i class="fa-solid fa-xmark"></i></div>        
+//         </li>
+//         `
+//     );
+//   }
   
-   const delayListItem = document.querySelectorAll(".delayListItem");
-  for (let i = 0; i <delayListData.length; i++) {
+//    const delayListItem = document.querySelectorAll(".delayListItem");
+//   for (let i = 0; i <delayListData.length; i++) {
     
-    delayListItem[i].querySelector(".delayDeleteButton").addEventListener("click",delayDeleteList);
-    function delayDeleteList()   {
-      delayListData.splice(i, 1);
-       createDelayList();
-          }
+//     delayListItem[i].querySelector(".delayDeleteButton").addEventListener("click",delayDeleteList);
+//     function delayDeleteList()   {
+//       delayListData.splice(i, 1);
+//        createDelayList();
+//           }
 
-    delayListItem[i].querySelector(".delayAddButton").addEventListener("click", delayAddList);
-       function delayAddList() 
-       {
-      const newTextFortodo = delayListData[i];         
-      todoListData.push(newTextFortodo);
+//     delayListItem[i].querySelector(".delayAddButton").addEventListener("click", delayAddList);
+//        function delayAddList() 
+//        {
+//       const newTextFortodo = delayListData[i];         
+//       todoListData.push(newTextFortodo);
            
-       delayListData.splice(i, 1);
-       createDelayList();    
-     createTodoList();
-    }
-  }
+//        delayListData.splice(i, 1);
+//        createDelayList();    
+//      createTodoList();
+//     }
+//   }
  
-};
+// };
+
+// logic on search input fiedl
+const searchInput = document.querySelector("#searchInput");
+const searchBtn = document.querySelector("#searchBtn");
+const clearBtn = document.querySelector("#clearBtn");
 
 searchBtn.addEventListener("click", handleSearchListItem);
 function handleSearchListItem() 
 {
   let searchTaskvalue = searchInput.value.toLowerCase().trim();
-  addListInTodoAfterFilterListText = searchTaskvalue;
+  // addListInTodoAfterFilterListText = searchTaskvalue;
 
   let todoItems = todoList.getElementsByTagName("li");
   let progressItems = progressList.getElementsByTagName("li");
   let completedItems = completedList.getElementsByTagName("li");
-  let delayItems = delayList.getElementsByTagName("li");
+  // let delayItems = delayList.getElementsByTagName("li");
   let signItems = signList.getElementsByTagName("li");
 
-  for (let i = 0; i < todoItems.length; i++) {
-    let text = todoItems[i].querySelector(".listText").textContent.toLowerCase();
-    let ismatch = text.includes(searchTaskvalue);
-    if (ismatch == true) {
+  for (let i = 0; i < todoListData.length; i++) {
+    let todotext = todoListData[i].title;
+     todotext = todotext.toLowerCase();
+    let todoIsmatch = todotext.includes(searchTaskvalue);
+    if (todoIsmatch == true) {
       todoItems[i].style.display = "flex";
     } else {
       todoItems[i].style.display = "none";
     }
   }
-  for (let i = 0; i < progressItems.length; i++) {
-    let progtext = progressItems[i].querySelector(".listText").textContent.toLowerCase();
-    let progressismatch = progtext.includes(searchTaskvalue);
+  for (let i = 0; i < progressListData.length; i++) {
+       let progresstext = progressListData[i].title;
+     progresstext = progresstext.toLowerCase();
+    let progressismatch = progresstext.includes(searchTaskvalue);
     if (progressismatch == true) {
       progressItems[i].style.display = "flex";
     } else {
       progressItems[i].style.display = "none";
     }
   }
-  for (let i = 0; i < completedItems.length; i++) {
-    let Comtext = completedItems[i].querySelector(".listText").textContent.toLowerCase();
-    let Complistismatch = Comtext.includes(searchTaskvalue);
-    if (Complistismatch == true) {
+  for (let i = 0; i < completedListData.length; i++) {
+    let completedtext = completedListData[i].title;
+     completedtext = completedtext.toLowerCase();
+    let completdIsmatch = completedtext.includes(searchTaskvalue);
+    if (completdIsmatch == true)
+       {
       completedItems[i].style.display = "flex";
     } 
     else{
       completedItems[i].style.display = "none";
     }
   }
-    for (let i = 0; i < delayItems.length; i++) {
-    let delayText = delayItems[i].querySelector(".listText").textContent.toLowerCase();
+    for (let i = 0; i < delayListData.length; i++) {
+    let delayText = delayListData[i].title;
+    delayText = delayText.toLowerCase();
     let delayListismatch = delayText.includes(searchTaskvalue);
     if (delayListismatch == true) {
       delayItems[i].style.display = "flex";
@@ -348,10 +347,12 @@ function handleSearchListItem()
       delayItems[i].style.display = "none";
     }
   }
-    for (let i = 0; i < signItems.length; i++) {
-    let signText = signItems[i].querySelector(".listText").textContent.toLowerCase();
-    let signListismatch = signText.includes(searchTaskvalue);
-    if (signListismatch == true) {
+    for (let i = 0; i < signListData.length; i++) 
+      {
+         let signtext = signListData[i].title;
+     signtext = signtext.toLowerCase();
+    let signIsmatch = signtext.includes(searchTaskvalue);
+       if (signIsmatch == true) {
       signItems[i].style.display = "flex";
     } 
     else {
